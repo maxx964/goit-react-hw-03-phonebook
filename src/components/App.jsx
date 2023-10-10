6cfbc38bcdb8882a76e77072f5c967bd53327403
maxx964 <127957496+maxx964@users.noreply.github.com>
2023-10-10 18:01:38 +0300

    ...

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,70 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React, { Component } from 'react';
+
+import { nanoid } from 'nanoid';
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
+
+
+  class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = ({ name,number}) => {
+    if (this.state.contacts.some((contact) => contact.name === name)) {
+      alert(`"${name}" is already in the list`);
+      return;
+  }
+const newContact = {
+  id: nanoid(),
+  name,
+  number,
 };
+
+
+  this.setState((prevState) => ({
+    contacts: [...prevState.contacts, newContact],
+  }));
+};
+
+  handleFilterChange = (e) => {
+   const { name, value } = e.target;
+   this.setState({ [name]: value });
+};
+
+  handleDeleteContact = (id) => {
+    this.setState((prevState) => ({
+      contacts: prevState.contacts.filter((contact) => contact.id !== id),
+    }));
+  };
+
+  render() {
+    const { contacts, filter } = this.state;
+    return(
+      <div>
+        <ContactForm onSubmit={this.handleSubmit}/>
+        <Filter value={filter} onChange={this.handleFilterChange} />
+        <ContactList
+          contacts={contacts}
+          onDeleteContact={this.handleDeleteContact}
+          filter={filter}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Filter/Filter.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './Filter.module.css';
+
+const Filter = ({ value, onChange }) => (
+    <div className={styles.filter}>
+    <h2 className={styles.h2}>Contacts</h2>
+    <h3 className={styles.h3}>Find contacts by name</h3>
+    <input
+      className={styles.input}
+      type="text"
+      name="filter"
+      placeholder="Enter first or last name"
+      value={value}
+      onChange={onChange}
+    />
+  </div>
+);
+
+export default Filter;
